e88c61e84cca698b8dd4d2ce5f6ecdbbcca44f00
Nikita Utkin <nickus.wanderer@gmail.com>
2023-12-06 15:08:40 +0300

    ENH: split to separate functions

--- pixelate.py
+++ pixelate.py
@@ -59,12 +59,33 @@ KEY = SVG(False, True, True)
 def image_resize(image: Image, size: int, new_width: int = 1000) -> Tuple[any, int]:
     new_width  = 1000
     pixelSize = int(new_width / int(size))
-    new_height = int(new_width * im.size[1] / im.size[0])
+    new_height = int(new_width * image.size[1] / image.size[0])
     image = image.resize((new_width, new_height), Image.NEAREST)
     return image, pixelSize
 
+def get_dmc_image(dmc_spaced, num_colours: int) -> Image:
+    dmc_image = Image.new('RGB', (len(dmc_spaced[0]), len(dmc_spaced))) #h, w
+    dmc_image.putdata([value for row in dmc_spaced for value in row])
+    dmc_image = dmc_image.convert('P', palette=Image.ADAPTIVE, colors = num_colours)
+
+    return dmc_image
+
+def update_svg_pattern(x_count: int, y_count: int, svg_pattern: SVG) -> None:
+    for x in range(0, x_count):
+            for y in range(0, y_count):
+                gen = get_neighbours([y, x], svg_pattern)
+                neighbours = []
+                for n in gen:
+                    neighbours += [n]
+                if svg_pattern[y][x] not in neighbours:
+                    mode = max(neighbours, key=neighbours.count)
+                    svg_pattern[y][x] = mode
+
+    return svg_pattern
+
 # c
-def main(input_file_path: Path, num_colours: int, size: int, out_directory: Path) -> None:
+def main(input_file_path: Path, num_colours: int, size: int, out_directory: Path,
+        svg_cell_size: int, key_size: int) -> None:
     if not out_directory.exists():              # size == count
         makedirs(out_directory)
 
@@ -84,51 +105,71 @@ def main(input_file_path: Path, num_colours: int, size: int, out_directory: Path
     image, pixelSize = image_resize(image, size)
 
     # 1, 2
-
-    d = DMC()
-    dmc_spaced = [[d.get_dmc_rgb_triple(im.getpixel((x, y))) for x in range(0, im.size[0], pixelSize)] for y in range(0, im.size[1], pixelSize)]
+    # d = DMC()
+    # dmc_spaced = [[dmc.get_dmc_rgb_triple(image.getpixel((x, y))) for x in range(0, image.size[0], pixelSize)] for y in range(0, image.size[1], pixelSize)]
+    # d == dmc!
+    dmc = DMC()
+    dmc_spaced = [
+        [
+            dmc.get_dmc_rgb_triple(image.getpixel((x, y)))
+            for x in range(0, image.size[0], pixelSize)
+        ]
+        for y in range(0, image.size[1], pixelSize)
+    ]
 
     # 3
 
-    dmc_image = Image.new('RGB', (len(dmc_spaced[0]), len(dmc_spaced))) #h, w
-    dmc_image.putdata([value for row in dmc_spaced for value in row])
+    # dmc_image = Image.new('RGB', (len(dmc_spaced[0]), len(dmc_spaced))) #h, w
+    # dmc_image.putdata([value for row in dmc_spaced for value in row])
 
-    # 4, 5
+    # # 4, 5
 
-    dmc_image = dmc_image.convert('P', palette=Image.ADAPTIVE, colors = num_colours)
+    # dmc_image = dmc_image.convert('P', palette=Image.ADAPTIVE, colors = num_colours)
+
+    # quantise the image with the required number of colours
+    #  a new image can then be created with row x column of palette indices
+    dmc_image = get_dmc_image(dmc_spaced, num_colours)
     x_count = dmc_image.size[0]
     y_count = dmc_image.size[1]
+    # a new palette can then be created with the dmc 'objects'
     svg_pattern = [[dmc_image.getpixel((x, y)) for x in range(x_count)] for y in range(y_count)]
 
     # 6
 
     palette = dmc_image.getpalette()
-    svg_palette = [d.get_colour_code_corrected((palette[i * 3], palette[i * 3 + 1], palette[i * 3 + 2])) for i in range(num_colours)]
+    # svg_palette = [dmc.get_colour_code_corrected((palette[i * 3], palette[i * 3 + 1], palette[i * 3 + 2])) for i in range(num_colours)]
+    svg_palette = [
+        dmc.get_colour_code_corrected((palette[i * 3], palette[i * 3 + 1], palette[i * 3 + 2]))
+        for i in range(num_colours)
+    ]
 
     # 7
-
-    if True:
-        for x in range(0, x_count):
-            for y in range(0, y_count):
-                gen = get_neighbours([y, x], svg_pattern)
-                neighbours = []
-                for n in gen:
-                    neighbours += [n]
-                if svg_pattern[y][x] not in neighbours:
-                    mode = max(neighbours, key=neighbours.count)
-                    svg_pattern[y][x] = mode
+    update_svg_pattern(x_count, y_count, svg_pattern)
+    # if True:
+    #     for x in range(0, x_count):
+    #         for y in range(0, y_count):
+    #             gen = get_neighbours([y, x], svg_pattern)
+    #             neighbours = []
+    #             for n in gen:
+    #                 neighbours += [n]
+    #             if svg_pattern[y][x] not in neighbours:
+    #                 mode = max(neighbours, key=neighbours.count)
+    #                 svg_pattern[y][x] = mode
 
     # 8
 
-    svg_cell_size = 10
+    # svg_cell_size = 10
     width = x_count * svg_cell_size
     height = y_count * svg_cell_size
+
     COL_SYM.prep_for_drawing(width, height)
     COL_SYM.mid_arrows(svg_cell_size, width, height)
     BLW_NSY.prep_for_drawing(width, height)
     BLW_NSY.mid_arrows(svg_cell_size, width, height)
     COL_NSY.prep_for_drawing(width, height)
+
     x = y = svg_cell_size # to allow drawing of midpoint arrows
+
     for row in svg_pattern:
         for colour_index in row:
             COL_SYM.add_rect(svg_palette, colour_index, x, y, svg_cell_size)
@@ -142,12 +183,12 @@ def main(input_file_path: Path, num_colours: int, size: int, out_directory: Path
 
     # 9
 
-    size = 40
-    KEY.prep_for_drawing(size * 13, size * len(svg_palette))
+    # key_size = 40
+    KEY.prep_for_drawing(key_size * 13, key_size * len(svg_palette))
     x = y = 0
     for i in range(len(svg_palette)):
-        KEY.add_key_colour(x, y, size, i, svg_palette[i])
-        y += size
+        KEY.add_key_colour(x, y, key_size, i, svg_palette[i])
+        y += key_size
 
     COL_SYM.save(Path(out_directory, 'col_sym.svg'))
     BLW_NSY.save(Path(out_directory, 'blw_sym.svg'))
@@ -162,6 +203,8 @@ if __name__ == '__main__':
     parse.add_argument("size", type=int, help="stitch count, number of stitches in x axis")
     parse.add_argument("-o","--out-directory", default=Path(Path(__file__).resolve().parent, "output"),
                         type=Path, help="output directory name")
+    parse.add_argument("-s", "--svg-cell-size", default=10, type=int, help="svg cell size")
+    parse.add_argument("-k", "--key-size", default=40, type=int, help="key size")
     args = parse.parse_args()
 
     main(*vars(args).values())
